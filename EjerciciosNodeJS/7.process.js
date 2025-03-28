097e39ccefc9d3ecb4c00acbcf9c2412ca77383d
lardevpro <lardevpro@gmail.com>
2025-03-29 00:06:00 +0100

    process

---
+++ EjerciciosNodeJS/7.process.js
@@ -0,0 +1,13 @@
+//argumentos de entrada (recoge los valores escritor )
+console.log(process.argv)
+
+//controlar eventos del proceso
+
+// process.exit(1) //con errores
+
+// //salir del proceso
+// process.on('exit',()=>{
+//     //limpiar recuros
+// })
+//ruta del directorio donde se ejecuta
+console.log(process.cwd())
