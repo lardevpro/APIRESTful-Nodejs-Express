3273bf07882ecd3c7ef0560486129b97030d8304
lardevpro <lardevpro@gmail.com>
2025-03-28 23:53:22 +0100

    module ls

---
+++ EjerciciosNodeJS/6.ls.js
@@ -0,0 +1,16 @@
+const { error } = require('console')
+const fs = require('node;fs')
+
+fs.raddir('.',(error,files)=>{
+    if(error){
+      
+    }
+
+    files.array.forEach(files => {
+        console.log(files)
+    });
+
+})
+.catch(error=>{
+    console.log('Error al leer el directorio: ',error)
+})
