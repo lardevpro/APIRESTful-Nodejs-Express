0c61a978518c329ac49ee46f80d2ec7405df1285
lardevpro <lardevpro@gmail.com>
2025-03-31 04:48:13 +0200

    first api with node.js finished

--- EjerciciosNodeJS/clase2/2.routing.js
+++ EjerciciosNodeJS/clase2/2.routing.js
@@ -1,6 +1,41 @@
 const http = require('node:http')
 
+const dittoJSON = require('./pokemon/ditto.json')
+
 const processRequest = (req, res) => {
+  const { method, url } = req
+
+  switch (method) {
+    case 'GET':
+      switch (url) {
+        case '/pokemon/ditto':
+          res.setHeader('Content-Type', 'application/json; charset=utf-8')
+          return res.end(JSON.stringify(dittoJSON))
+        default:
+          res.statusCode = 404
+          res.setHeader('Content-Type', 'text/html; charset=utf-8')
+          return res.end('<h1>404</h1>')
+      }
+    case 'POST':
+      switch (url) {
+        case '/pokemon': {
+          let body = ''
+          // escuchar el evento
+          req.on('data', chunk => {
+            body += chunk.toString()
+          })
+
+          req.on('end', () => {
+            const data = JSON.parse(body)
+            // código para guardar el pokemon
+          })
+          break
+        }
+        default:
+          res.statusCod = 404
+          return res.end('404 Not Found')
+      }
+  }
 }
 
 const server = http.createServer(processRequest)
