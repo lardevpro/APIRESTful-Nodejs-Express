4f58b8d69fddbaed07c7ab04228a61ec4c208d2e
lardevpro <lardevpro@gmail.com>
2025-03-31 02:13:14 +0200

    frist http request finished

--- EjerciciosNodeJS/clase2/1.http.js
+++ EjerciciosNodeJS/clase2/1.http.js
@@ -1,12 +1,25 @@
+const fs = require('node:fs')
 const http = require('node:http')
 
 const desiredPort = process.env.PORT ?? 1234
 
 const processRequest = (req, res) => {
+  res.setHeader('Content-Type', 'text/plain; charset=utf-8')
+
   if (req.url === '/') {
     res.statusCode = 200 // recibido correctamente
-    res.setHeader('Content-Type', 'text/plain; charset=utf-8')
-    res.end('Hola añudadod amigo mío')
+    res.end('Esta es la página Home')
+  } else if (req.url === '/image-node-js.png') {
+    fs.readFile('./node-js.png', (err, data) => {
+      if (err) {
+        res.statusCode = 500
+        res.end('<h1>500 Internal Server Error</h1>')
+      } else {
+        res.statusCode = 200
+        res.setHeader('Content-Type', 'image/png')
+        res.end(data)
+      }
+    })
   } else if (req.url === '/contacto') {
     res.statusCode = 200
     res.setHeader('Content-Type', 'text/plain; chrset=utf-8')
